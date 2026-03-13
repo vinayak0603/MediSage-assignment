f517b6e674a3bcc4924227a81a443b5b87da13b1
vinayak andhere <vinayakandhere4@gmail.com>
2026-03-13 10:05:45 +0530

    backend url added

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://api-assignment-nu.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
